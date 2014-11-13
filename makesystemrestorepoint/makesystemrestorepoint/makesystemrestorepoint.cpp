e35e796a7c5ff43b6b3255af7f528ba95d091459
nanoko <yy845jp@gmail.com>
2014-11-13 09:04:25 +0900

    version up test

--- makesystemrestorepoint/makesystemrestorepoint/makesystemrestorepoint.cpp
+++ makesystemrestorepoint/makesystemrestorepoint/makesystemrestorepoint.cpp
@@ -13,10 +13,17 @@
 #include "stdafx.h"
 #include "makesystemrestorepoint.h"
 
-#define	RestorePtrType	APPLICATION_INSTALL;
-#define	RestoreDescription	_T("Demonstration Restore Point")
+#define	DefaultRestorePtrType		APPLICATION_INSTALL;
+#define	DefaultRestoreDescription	_T("makesystemrestorepoint")
 
-#define MAX_LOADSTRING 100
+#define MAX_LOADSTRING 40
+#define	MAX_STRINGS	256
+
+#define	SP_OK		0
+#define	SP_NG1		-1
+#define	SP_NG2		-2
+#define	SP_HELP		-64
+#define	SP_OPTERR	-128
 
 HINSTANCE hInst;
 TCHAR szTitle[MAX_LOADSTRING];
@@ -56,6 +63,8 @@ static LPWSTR atow(LPCSTR src)
 	return buf;
 }
 
+#endif /* ndef UNICODE */
+
 static LPSTR wtoa(LPCWSTR src)
 {
 	LPSTR buf;
@@ -81,7 +90,6 @@ static LPSTR wtoa(LPCWSTR src)
 
 	return buf;
 }
-#endif /* ndef UNICODE */
 
 static LPTSTR *parse_args(LPCTSTR args_t, int *argc)
 {
@@ -155,46 +163,172 @@ static void free_args(int argc, LPTSTR *argv)
 #endif
 }
 
-int tMakeSystemRestore(int argc, LPTSTR *argv) {
-	RESTOREPOINTINFO RstPt;
-	STATEMGRSTATUS MgrStat;
+int	iSilent = 0;
 
-	LPTSTR	szRestoreMsg;
+int msgbox(HINSTANCE hInst, int iMsgid, int iButtonid) {
+	TCHAR	szMsg[MAX_STRINGS];
+	TCHAR	szTitle[MAX_STRINGS];
 
-	if (argc < 2) {
-		szRestoreMsg = RestoreDescription;
-	} else { 
-		if (wcscmp(argv[1], _T("/?")) == 0) {
-			MessageBox(NULL, _T("システムの復元作成プログラム by ななこっち★\nUsage このプログラム.exe [システムの復元ポイントに残す任意のメッセージ]"), _T("システムの復元作成プログラム"), MB_ICONINFORMATION);
-			return 1;
-		}
-		szRestoreMsg = argv[1];
+	LoadString(hInst, iMsgid, szMsg, sizeof(szMsg) / sizeof(szMsg[0]));
+	LoadString(hInst, IDS_APP_TITLE, szTitle, sizeof(szTitle) / sizeof(szTitle[0]));
+	MessageBox(NULL, szMsg, szTitle, iButtonid);
+	return SP_NG1;
+}
+
+int infobox(HINSTANCE hInst, int iMsgid) {
+	if (!iSilent) {
+		msgbox(hInst, iMsgid, MB_OK | MB_ICONINFORMATION);
 	}
+	return SP_NG1;
+}
 
-	if (wcslen(szRestoreMsg) > 40) {
-		MessageBox(NULL, _T("そんなに長いメッセージは無理なの"), _T("システムの復元作成プログラム"), MB_ICONINFORMATION);
-		return 1;
+int errmsg(HINSTANCE hInst, int iMsgid) {
+	if (!iSilent) {
+		msgbox(hInst, iMsgid, MB_OK | MB_ICONERROR);
 	}
+	return SP_NG1;
+}
+
+int erropt(HINSTANCE hInst, int iMsgid) {
+	msgbox(hInst, iMsgid, MB_OK | MB_ICONWARNING);
+	return SP_OPTERR;
+}
+
+RESTOREPOINTINFO RstPt;
+STATEMGRSTATUS MgrStat;
+
+INT64 tMakeSystemBegin(int iMode, int iSilent, LPTSTR szRestoreMsg) {
+
 	RstPt.dwEventType = BEGIN_SYSTEM_CHANGE;
-	RstPt.dwRestorePtType = RestorePtrType;
-	wcscpy_s((wchar_t *)RstPt.szDescription, wcslen(szRestoreMsg) + 1, szRestoreMsg);
+	RstPt.dwRestorePtType = iMode;
+	_tcscpy_s((wchar_t *)RstPt.szDescription, _tcslen(szRestoreMsg) + 1, szRestoreMsg);
 
-	if (!SRSetRestorePoint(&RstPt, &MgrStat))
-	{
-		MessageBox(NULL, _T("検索失敗"), _T("＠＠"), MB_ICONINFORMATION);
-		return 1;
+	if (!SRSetRestorePoint(&RstPt, &MgrStat)) {
+		return -1;
 	}
+	return MgrStat.llSequenceNumber;
+}
 
+int tMakeSystemEnd(int iMode, int iSilent, INT64 iSeq) {
 	RstPt.dwEventType = END_SYSTEM_CHANGE;
-	RstPt.llSequenceNumber = MgrStat.llSequenceNumber;
+	RstPt.llSequenceNumber = iSeq;
+	RstPt.dwRestorePtType = iMode;
 
-	if (!SRSetRestorePoint(&RstPt, &MgrStat))
-	{
-		// システムの復元ポイントの失敗時　何もしない
-			MessageBox(NULL, _T("作成失敗"), _T("＠＠"), MB_ICONINFORMATION);
-			return 1;
+	if (!SRSetRestorePoint(&RstPt, &MgrStat)) {
+		return 1;
 	}
 	return 0;
+}
+
+int optcmp(HINSTANCE hInst, LPTSTR argv, int iMsgid) {
+	TCHAR	szTmp[MAX_STRINGS];
+	TCHAR	szTmp2[MAX_STRINGS];
+	TCHAR	*szTok;
+	TCHAR	*next_token = NULL;
+	int		iFlg = 0;
+
+	LoadString(hInst, iMsgid, szTmp, sizeof(szTmp) / sizeof(szTmp[0]));
+	_tcsupr_s(szTmp, MAX_STRINGS);
+	_tcscpy_s(szTmp2, MAX_STRINGS, argv);
+	_tcsupr_s(szTmp2, MAX_STRINGS);
+
+	szTok = _tcstok_s(szTmp, _T(","), &next_token);
+	do {
+		if (_tcscmp(szTok, szTmp2) == 0) {
+			iFlg = 1;
+		}
+	} while ((szTok = _tcstok_s(NULL, _T(","), &next_token)) != NULL);
+	return iFlg;
+}
+
+
+int tMakeSystemRestore(HINSTANCE hInst, int argc, LPTSTR *argv) {
+	LPTSTR	szRestoreMsg = DefaultRestoreDescription;
+	int	i;
+	int iFlg = 0;
+	int iMode = 0;
+	int iRet = SP_OK;
+	INT64 iSeq = 0;
+
+	for (i = 1; i < argc; i++) {
+		if (optcmp(hInst, argv[i], IDS_SP_OPT_HELP)) {
+			infobox(hInst, IDS_SP_HELP);
+			return SP_HELP;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_SILENT)) {
+			iSilent = 1;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_BEGIN)) {
+			if (iFlg) {
+				return erropt(hInst, IDS_SP_ERR_OPT);
+			}
+			iFlg = 1;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_END)) {
+			if (iFlg) {
+				return erropt(hInst, IDS_SP_ERR_OPT);
+			}
+			iFlg = 2;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_INSTALL)) {
+			if (iMode) {
+				return erropt(hInst, IDS_SP_ERR_OPT);
+			}
+			iMode = APPLICATION_INSTALL;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_UNINSTALL)) {
+			if (iMode) {
+				return erropt(hInst, IDS_SP_ERR_OPT);
+			}
+			iMode = APPLICATION_UNINSTALL;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_DEVICE)) {
+			if (iMode) {
+				return erropt(hInst, IDS_SP_ERR_OPT);
+			}
+			iMode = DEVICE_DRIVER_INSTALL;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_MODIFY)) {
+			if (iMode) {
+				return erropt(hInst, IDS_SP_ERR_OPT);
+			}
+			iMode = MODIFY_SETTINGS;
+		}
+		else if (optcmp(hInst, argv[i], IDS_SP_OPT_CANCEL)) {
+			if (iMode) {
+				return erropt(hInst, IDS_SP_ERR_OPT);
+			}
+			iMode = CANCELLED_OPERATION;
+		}
+		else {
+			szRestoreMsg = argv[i];
+			if (_tcslen(szRestoreMsg) > MAX_LOADSTRING) {
+				return erropt(hInst, IDS_SP_ERR_LONG);
+			}
+		}
+	}
+
+	if (!iMode) {
+		iMode = DefaultRestorePtrType;
+	}
+	if (!iFlg || iFlg == 1) {
+		if ((iSeq = tMakeSystemBegin(iMode, iSilent, szRestoreMsg)) == -1) {
+			return errmsg(hInst, IDS_SP_ERR_NG1);
+		}
+	}
+	if (!iFlg || iFlg == 2) {
+		if (iFlg == 2) {
+			iSeq = atoi(wtoa(szRestoreMsg));
+			if (iSeq < 1) {
+				return erropt(hInst, IDS_SP_ERR_NUM);
+			}
+		}
+		if ((iSeq = tMakeSystemEnd(iMode, iSilent, iSeq)) == -1) {
+			return errmsg(hInst, IDS_SP_ERR_NG2);
+		}
+		infobox(hInst, IDS_SP_OK);
+	}
+	return (iFlg == 1 ? iSeq : SP_OK);
 }
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
@@ -202,11 +336,12 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	_In_ LPTSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	int iRet, argc = 0;
+	int argc = 0, iRet = 0;
 	LPTSTR *argv;
 	argv = parse_args(GetCommandLine(), &argc);
 
-	iRet = tMakeSystemRestore(argc, argv);
+	iRet = (int)tMakeSystemRestore(hInstance, argc, argv);
 
 	free_args(argc, argv);
+	return iRet;
 }
